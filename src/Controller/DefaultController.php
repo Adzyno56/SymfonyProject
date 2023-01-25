a7501d5d9b65bb06a6c01e9c2ddb86a06aa199e2
Tikhonov Grigoriy <tikhonovg.99@gmail.com>
2023-01-25 23:26:31 +0500

    #Создание первого контроллера - добавить продукт#

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class DefaultController extends AbstractController
 {
+    //private ManagerRegistry $doctrine;
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine) {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function index(): Response
     {
+        $entityManager = $this->doctrine->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+        dd($productList);
+
         return $this->render('main/default/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/product-add", name="product_add")
+     */
+    public function productAdd(): Response
+    {
+        $product = new Product();
+        $product->setTitle('product_'.rand(1,100));
+        $product->setDescription('smth');
+        $product->setPrice(10);
+        $product->setQuantity(1);
+
+        $entityManager = $this->doctrine->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+        return $this->redirectToRoute('homepage');
     }
 }
